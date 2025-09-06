fdb3c920696010022cef7082081e3b5f21009056
Ardhendu Sekhar Mishra <ardhendupurnendu@gmail.com>
2025-09-06 11:49:45 +0530

    book middlewares

--- middlewares/books.js
+++ middlewares/books.js
@@ -5,17 +5,6 @@ const { Book } = require('../db');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 async function bookMiddleware(req, res, next) {
-    try {
-        const token = req.headers.authorization;
-        if (!token) {
-            return res.status(401).json({ error: "Access denied. No token provided." });
-        }
-
-        const words = token.split(' ');
-        const decoded = jwt.verify(words[1], JWT_SECRET);
-        if (decoded.email) {
-            req.email = decoded.email;
-
             // If request has bookId (like update/delete), check if book exists
             if (req.params.bookId) {
                 const book = await Book.findById(req.params.bookId);
